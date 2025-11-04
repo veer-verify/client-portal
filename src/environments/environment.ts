e44d733c19cb729b7641201bc930b3833aec6afc
veer-verify <veer.verify@gmail.com>
2025-11-04 12:18:19 +0530

    themes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,7 @@
 const dev_url: string = 'https://usstaging.ivisecurity.com';
 const local_url: string = 'http://192.168.0.151:3006';
 export const environment = {
-  production: false,
-
+  // production: false,
   // authUrl: `${dev_url}:8922/userDetails`,
   // commonUrl: `${dev_url}:8844/metadata`,
   // commonDownUrl: `${dev_url}:8001/common`,
@@ -29,12 +28,30 @@ export const environment = {
   rulesUrl: `${dev_url}/proximityAdsRules `,
   insightsUrl: `${dev_url}/insights`,
   timelapseUrl: `${dev_url}/timeLapse`,
-  sensorUrl:`${dev_url}/sensors`,
-  simsUrl:`${dev_url}/simDevices`,
+  sensorUrl: `${dev_url}/sensors`,
+  simsUrl: `${dev_url}/simDevices`,
   faqUrl: `${dev_url}/faq`,
   inventoryUrl: `${dev_url}/inventory`,
   helpdeskUrl: `${dev_url}/supportRequests`,
   incidentsUrl: `${dev_url}/guard_monitoring`,
+
+  theme: {
+    logo: 'assets/themes/ivis_logo_white.png',
+    headerLogo: 'assets/themes/ivis_logo_black.png',
+    accordianLogo: 'assets/icons/eye.svg',
+    activeLogo: 'assets/icons/eye-blue.svg',
+    inActiveLogo: 'assets/icons/eye-red.svg',
+
+    detail: {
+      address: 'IVIS Security, Inc',
+      city: '3945 W Cheyenne Ave #204',
+      state: 'North Las Vegas, NV 89032',
+      country: 'United States',
+      phone: '+1 (844) 438-4847',
+      email: 'support@ivisecurity.com',
+    }
+  }
+
 };
 
 /*
