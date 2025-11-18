31da43fdb69f5dba36f1e88737203f1549081acb
veer-verify <veer.verify@gmail.com>
2025-11-18 18:57:14 +0530

    verifai

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../services/auth/authservice.service';
 import { StorageService } from '../services/storage.service';
 import { AlertService } from '../services/alertservice/alert-service.service';
 import { ApiService } from '../services/api.service';
+import { ProximityService } from '../services/proximity.service';
 
 @Component({
   selector: 'app-signin',
@@ -12,15 +13,16 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
-constructor(
+  constructor(
     private fb: FormBuilder,
     private authservice: AuthService,
     private alertService: AlertService,
     private apiService: ApiService,
     private router: Router,
     public storageService: StorageService,
-    private cdr: ChangeDetectorRef
-    ) { }
+    private cdr: ChangeDetectorRef,
+    private proxSer: ProximityService
+  ) { }
 
   loginForm!: FormGroup;
   ngOnInit() {
@@ -41,7 +43,7 @@ constructor(
     });
   }
 
-    manageUserSession() {
+  manageUserSession() {
     this.authservice.manageUserSession('logIn').subscribe({
       next: (res: any) => {
         localStorage.setItem('sId', JSON.stringify(res.sessionId ?? ''));
@@ -51,53 +53,39 @@ constructor(
 
   showLoader: boolean = false;
   login() {
-
     if (!this.loginForm.valid) return;
     this.showLoader = true;
     this.authservice.loginNew(this.loginForm.value.userName, this.loginForm.value.password).subscribe({
       next: (res: any) => {
-            this.showLoader = false;
+        this.showLoader = false;
         if (res.Status === 'Success') {
           this.storageService.storeEncrData('user', res);
           localStorage.setItem('acTok', JSON.stringify(res.AccessToken || ''));
           this.authservice.isLoggedin.next(true);
           this.authservice.getAuthStatus();
           this.manageUserSession();
-                    if(res.Status == 'Success' && res.firstTime == 'F') {
-            this.router.navigateByUrl('/guard');
-          } else if(res.Status == 'Success' && res.firstTime == 'T') {
-            // this.dialogType = 'firstTime';
-          }
+          this.getMetadata();
+          this.router.navigateByUrl('/guard');
         } else {
-          this.alertService.error("error", res.message);
+          this.alertService.sweetError(res.message);
         }
       },
       error: (err: any) => {
-        this.alertService.error("error", err.message);
+        this.alertService.sweetError(err.message);
       }
     })
   }
-
-  // manageUserSession() {
-  //   this.loginSer.manageUserSession('logIn').subscribe({
-  //     next: (res) => {
-  //       sessionStorage.setItem('sId', JSON.stringify(res.sessionId ?? ''));
-  //       this.storageSer.session_sub.next(res.sessionId ?? '');
-  //     }
-  //   })
-  // }
 
   showPassword: boolean = false;
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
 
-  // getMetadata() {
-  //   this.metadata_service.getMetadata().subscribe({
-  //     next: (res: any) => {
-  //       this.storageSer.metadat_sub = res
-  //     }
-  //   })
-  // }
+  getMetadata() {
+    this.proxSer.getMetadata().subscribe((res: any) => {
+      // localStorage.setItem('metaData', JSON.stringify(res));
+      this.storageService.storeEncrData('metaData', res);
+    })
+  }
 
 }
